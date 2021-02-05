32ca7bc259ff8ba9e6619354389e0c603fe56ac7
Danny Betancourt <DBetancourt@talentpath.com>
2021-02-05 00:25:58 -0600

    Added a test suite for fetchQuiz which will eventually receive the quiz object from the Rails api

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,65 +1,98 @@
+import React from 'react'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import fetchQuiz from './api/fetchQuiz'
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+class Main extends React.Component
+{
+  constructor(props)
+  {
+    super(props)
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+    this.state = { answer1: "", answer2: "", answer3: "", answer4: "" }
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+    this.handleQuestionOne = this.handleQuestionOne.bind(this)
+    this.handleQuestionTwo = this.handleQuestionTwo.bind(this)
+    this.handleQuestionThree = this.handleQuestionThree.bind(this)
+    this.handleQuestionFour = this.handleQuestionFour.bind(this)
+  }
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+  componentDidMount()
+  {
+    this.questions = fetchQuiz()
+  }
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+  handleQuestionOne(event)
+  {
+    this.setState({ answer1: event.target.value })
+    console.log(this.state)
+  }
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+  handleQuestionTwo(event)
+  {
+    this.setState({ answer2: event.target.value })
+    console.log(this.state)
+  }
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+  handleQuestionThree(event)
+  {
+    this.setState({ answer3: event.target.value })
+    console.log(this.state)
+  }
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+  handleQuestionFour(event)
+  {
+    this.setState({ answer4: event.target.value })
+    console.log(this.state)
+  }
+
+  render()
+  {
+    return (
+      <div className={styles.container}>
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            Point 'n' Pick!
+          </h1>
+
+          <div className={styles.grid}>
+            <div onChange={this.handleQuestionOne} className={styles.card}>
+                <h3>&larr; Which team most recently won the Super Bowl? &rarr;</h3>
+                <input type="radio" value="1" name="one" /> First
+                <input type="radio" value="2" name="one" /> Second
+                <input type="radio" value="3" name="one" /> Third
+                <input type="radio" value="4" name="one" /> Fourth
+            </div>
+
+            <div onChange={this.handleQuestionTwo} className={styles.card}>
+                <h3>&larr; Which player had the highest touchdowns scored in 2020? &rarr;</h3>
+                <input type="radio" value="1" name="two" /> First
+                <input type="radio" value="2" name="two" /> Second
+                <input type="radio" value="3" name="two" /> Third
+                <input type="radio" value="4" name="two" /> Fourth
+            </div>
+
+            <div onChange={this.handleQuestionThree} className={styles.card}>
+                <h3>&larr; Which team has the most Super Bowl wins? &rarr;</h3>
+                <p>Discover and deploy boilerplate example Next.js projects.</p>
+              </div>
+
+            <div onChange={this.handleQuestionFour} className={styles.card}>
+                <h3>&larr; Which player has had the most injuries? &rarr;</h3>
+                <p>
+                  Instantly deploy your Next.js site to a public URL with Vercel.
+                </p>
+            </div>
+          </div>
+        </main>
+      </div>
+    )
+  }
 }
+
+export default Main
